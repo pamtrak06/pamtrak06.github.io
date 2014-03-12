97f78d45f5c7b9f688c64d480d85c6d846ea91a6
jp <jpTlse@gmail.com>
2014-03-12 08:36:02 +0100

    updated

--- mapPower/mappower.js
+++ mapPower/mappower.js
@@ -499,7 +499,7 @@ buildMapWms = (function(mapUrl, mapWmsDiv, layer, style, time, elevation){
     dataLayer = new ol.layer.Image({
         source: new ol.source.ImageWMS({
             url: mapUrl + sep + 'time=' + time + '&elevation=' + elevation,
-            //crossOrigin: 'anonymous',
+            crossOrigin: 'null',
             attributions: [new ol.Attribution({
                 html: '&copy; ' +
                 '<a href="http://dd.meteo.gc.ca/doc/LICENCE_GENERAL.txt' +
@@ -599,7 +599,6 @@ buildMapFromDiv = (function(){
         buildMapWmtsFromDiv();
     }
 });
-
 
 updateService = (function(){
     if (service == 'WMS') {
