0355e8ed4db2c3b3b4bcec9bbba3aa21b3ac6bc4
pamtrak06 <pamtrak06@gmail.com>
2016-01-24 11:28:21 +0100

    test progress bar, add google analytics

--- mapPower/mappower.js
+++ mapPower/mappower.js
@@ -1,3 +1,70 @@
+/**
+ * Renders a progress bar.
+ * @param {Element} el The target element.
+ * @constructor
+ */
+function Progress(el) {
+  this.el = el;
+  this.loading = 0;
+  this.loaded = 0;
+}
+
+
+/**
+ * Increment the count of loading tiles.
+ */
+Progress.prototype.addLoading = function() {
+  if (this.loading === 0) {
+    this.show();
+  }
+  ++this.loading;
+  this.update();
+};
+
+
+/**
+ * Increment the count of loaded tiles.
+ */
+Progress.prototype.addLoaded = function() {
+  setTimeout(function() {
+    ++this.loaded;
+    this.update();
+  }.bind(this), 100);
+};
+
+
+/**
+ * Update the progress bar.
+ */
+Progress.prototype.update = function() {
+  var width = (this.loaded / this.loading * 100).toFixed(1) + '%';
+  this.el.style.width = width;
+  if (this.loading === this.loaded) {
+    this.loading = 0;
+    this.loaded = 0;
+    setTimeout(this.hide.bind(this), 500);
+  }
+};
+
+
+/**
+ * Show the progress bar.
+ */
+Progress.prototype.show = function() {
+  this.el.style.visibility = 'visible';
+};
+
+
+/**
+ * Hide the progress bar.
+ */
+Progress.prototype.hide = function() {
+  if (this.loading === this.loaded) {
+    this.el.style.visibility = 'hidden';
+    this.el.style.width = 0;
+  }
+};
+
 
 // --------------------------------------------
 // Store an object
@@ -572,8 +639,20 @@ buildMapWms = (function(mapUrl, mapWmsDiv, layer, style, time){
             extent: EXTENT_
         })
     });
+    /*
+    var progress = new Progress(document.getElementById('progress'));
     
-    
+    dataLayer.on('tileloadstart', function(event) {
+      progress.addLoading();
+    });
+
+    dataLayer.on('tileloadend', function(event) {
+      progress.addLoaded();
+    });
+    dataLayer.on('tileloaderror', function(event) {
+      progress.addLoaded();
+    });
+    */
     
     // Build layer of the world
     worldLayer = buildLayerWorld();
